646ff9977327c8f7db0ae052cab32f9485e03a12
Jonathon Chambers <jonathon@jonathonchambers.com>
2019-05-12 19:26:54 -0400

    Removed Test Files

--- test.py
+++
@@ -1,9 +0,0 @@
-from blabel import LabelWriter
-
-label_writer = LabelWriter("test.html", default_stylesheets=("test.css",), items_per_page=30)
-records= [
-]
-for i in range(1,30):
-    records.append(dict(sample_id="LR"+str(i).zfill(2)+"GVDY", sample_name="Sample " + str(i)))
-
-label_writer.write_labels(records, target='qrcode_and_label.pdf')
