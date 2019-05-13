6dc8ad9e0657c4f2afac651161b0d5304925df37
Jonathon Chambers <jonathon@jonathonchambers.com>
2019-05-12 23:34:59 -0400

    Update Commit

--- processors.py
+++ processors.py
@@ -15,24 +15,12 @@ def writeLabels(excelFile, templateFile, stylesheet=None):
     assets = list()
     try:
         dataframe = pandas.read_excel(excelFile,dtype='str')
-        values = dataframe.get_values()
         
     except:
         print("Unknown Exception: Failed to load spreadsheet.")
         return False
-    
-    for record in values:
-        aTag = record[0]
-        model = record[1]
-        location = record[2]
 
-        tempAsset = Asset()
-        tempAsset.Asset_Tag = aTag
-        tempAsset.Model = model
-        tempAsset.Location = location
-
-        assets.append(tempAsset)
-
+        records = dataframe.to_dict(orient='record')
         label_writer = LabelWriter(templateFile, default_stylesheets=(stylesheet))
         label_writer.write_labels(assets, target="export.pdf")
     return True
